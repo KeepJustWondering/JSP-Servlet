f5b798662821f2c29f1698adf23507946e31a38f
KeepJustWondering <wodnjs3428@naver.com>
2020-05-09 22:26:46 +0900

    TempFilter.java생성,student2.java & studentCon.java & web.xml 파일 수정
    TempFilter를 생성하여 받아오는 정보에 대하여 한글화를 함.
    student2와 studentCon 파일에 있던 request한글화 명령어를 제거
    web.xml 파일에서 filter 매핑

--- JSP-Servlet2/src/com/servlet/student2.java
+++ JSP-Servlet2/src/com/servlet/student2.java
@@ -15,7 +15,7 @@ public class student2 extends HttpServlet {
    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.setCharacterEncoding("UTF-8");
+		
 		response.setContentType("text/html; charset=UTF-8");
 		
 		
