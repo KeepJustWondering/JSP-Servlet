4da68da39e03958f4ecc1aeb18eff2e59cfdd1c6
KeepJustWondering <wodnjs3428@naver.com>
2020-05-09 21:32:20 +0900

    student2.java생성, web.xml 수정
    web.xml에서 서블릿 매핑을 이용하여 @WebServlet("/ST") 구문없이 매핑을 하였으며 내장객체인 config객체를
    사용하여 학번과, 주로 사용하는 언어의 정보를 받아 옴.
    추가적으로 한글화 까지 적용 함.

---
+++ JSP-Servlet2/src/com/servlet/student2.java
@@ -0,0 +1,38 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class student2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		
+		PrintWriter out = response.getWriter();
+		String number = getServletConfig().getInitParameter("s_number");
+		String language = getServletConfig().getInitParameter("p_language");
+		
+		out.print("학번 : "+number+"<br>");
+		out.print("주로 사용하는 언어 : "+language+"<br>");
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
